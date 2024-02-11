b47cc82cca691a45273694e080aad0ba94def209
Jose-YP <joseaw2003@gmail.com>
2024-02-10 19:02:18 -0500

    Added AI preferences

--- Code/ResourceMakers/Entity/EnemyMaker.gd
+++ Code/ResourceMakers/Entity/EnemyMaker.gd
@@ -9,16 +9,44 @@ class_name Enemy
 @export var Boss: bool
 @export var Revivable: bool
 
-@export_category("AI")
-@export var AICode: Script #Code that determines what the user will perceive, when and what after
+@export_group("AI")
 @export var AICodePath: String
 @export var Priorities: Array = ["Attack","Buff","Debuff","Element",
 "Condition","Heal","Aura","Summon","Ailment","Misc"] #Reordered depending on user
 @export var PriorityChance: Array = [50,50,50,50,
 50,50,50,50,50,50] #When multiple conditions are met use chances that are before 100
+
+@export_subgroup("Resource Preferences")
+@export_range(0,1,.05) var selfHPPreference: float = .9
+@export_range(0,1,.05) var allyHPPreference: float = .9
+@export_range(0,1,.05) var oppHPPreference: float = .9
+
+@export_subgroup("Ailment Preferences")
+@export_range(0,3) var selfAilmentPreference: int = 0
+@export_range(0,3) var allyAilmentPreference: int = 0
+@export_range(0,3) var oppAilmentPreference: int = 0
+@export_flags("Overdrive","Poison","Reckless","Exhausted","Rust","Stun","Curse","Protected","Dumbfounded",
+"Miserable","Worn Out", "Explosive","XSoft") var favoredAilments: int = 0
+@export_range(0,3) var selfXSoftPreference: int = 0
+@export_range(0,3) var allyXSoftPreference: int = 0
+@export_range(0,3) var oppXSoftPreference: int = 0
+@export_flags("Fire","Water","Elec","Slash","Crush","Pierce") var favoredXSoftTypes: int
+
+@export_subgroup("Stat Buff Preferences")
+@export_range(0,.6,.05) var selfBuffAmmountPreference: float = .5
+@export_range(0,.6,.05) var allyBuffAmmountPreference: float = .5
+@export_range(0,.6,.05) var oppBuffAmmountPreference: float = .5
+@export_range(-.6,0,.05) var selfDebuffAmmountPreference: float = -.5
+@export_range(-.6,0.05) var allyDebuffAmmountPreference: float = -.5
+@export_range(-.6,0,.05) var oppDebuffAmmountPreference: float = -.5
+
+@export_subgroup("Other Buff Preferences")
+@export_flags("Charge","Amp","Targetted","Endure","Peace","Lucky","Reflect","Absorb","Devoid","AnotherTurn") var favoredCondition: int = 763
+@export_enum("Fire","Water","Elec","Neutral") var selfFavoredElement = "Neutral"
+@export_enum("Fire","Water","Elec","Neutral") var allyFavoredElement = "Neutral"
+@export_enum("Fire","Water","Elec","Neutral") var oppFavoredElement = "Neutral"
+
+@export_subgroup("Other Preferences")
 @export_flags("BodyBroken","WillWrecked","DoubleCrits","LowTP") var favoredAuras: int = 15
-#If they're present they'll be prioritized
 @export var favoredAlly: String
 @export_enum("DREAMER","Lonna","Damir","Pepper", "None") var hatedOpponent: String = "None"
-#Placeholder
-@export_enum("Random","Single Strike", "Pick off","Support", "Debuff") var AIType: String = "Random"
